464da82af7b17a00f32f79bb59cff8a1fbbfdd98
singerpo <852361303@qq.com>
2022-05-18 18:35:47 +0800

    update

--- spring-amine/src/main/java/com/face/singleton/Mgr02.java
+++ spring-amine/src/main/java/com/face/singleton/Mgr02.java
@@ -5,7 +5,9 @@ package com.face.singleton;
  * 懒汉式-同步代码块-双重检查
  */
 public class Mgr02 {
-
+	/**
+	 * volatile 禁止指令重排序
+	 */
 	private static volatile Mgr02 mgr02;
 
 	private Mgr02(){
@@ -14,6 +16,7 @@ public class Mgr02 {
 
 	public static Mgr02 getInstance(){
 		if(mgr02 == null){
+			// 双重检查
 			synchronized (Mgr02.class){
 				if(mgr02 == null){
 					mgr02 = new Mgr02();
